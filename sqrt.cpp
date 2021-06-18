c44e3373528dfdbcac82d5b73b92903c0500866c
srivatsan1892 <63724241+srivatsan1892@users.noreply.github.com>
2021-06-18 19:51:37 +0530

    Create sqrt.cpp

---
+++ sqrt.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int a=sqrt(n);
+	    int b=floor(a);
+	    cout<<b<<endl;
+	}
+	return 0;
+}
